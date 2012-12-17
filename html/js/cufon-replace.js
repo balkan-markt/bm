c4d6cceb8cb8ff0dec2ad57d6460dee5977ca069
Ognyan <oggo@oggo-laptop.(none)>
2012-12-17 14:19:28 +0100

    html template and the project structure

---
+++ html/js/cufon-replace.js
@@ -0,0 +1,16 @@
+Cufon.replace('#header .nav, h2', { 
+				  fontFamily: 'Myriad Pro',
+				  hover:true
+});
+
+Cufon.replace('h2, h3, .extra-banner', { 
+				  fontFamily: 'Myriad Pro Bold'
+});
+
+Cufon.replace('h1', { 
+				  fontFamily: 'Aachen BT'
+});
+
+Cufon.replace('.top-propose div', { 
+				  fontFamily: 'Aachen BT Bold'
+});
